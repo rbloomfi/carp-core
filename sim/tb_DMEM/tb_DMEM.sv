1c318727d2602028fc38b5d65a66c78c92e20293
d <dsandall@calpoly.edu>
2025-11-15 19:51:15 -0800

    DMEM issue resolved. problem was that i didn't notice the data memory offset (bruh)

--- sim/tb_DMEM/tb_DMEM.sv
+++ sim/tb_DMEM/tb_DMEM.sv
@@ -6,6 +6,8 @@ module tb_DMEM ();
   logic [1:0] BYTE_SEL;
   logic [13:0] ADDR;
   logic [31:0] DATA_IN;
+  wire [31:0] dout;
+  reg [31:0] myvar;
   logic CLK;
 
   DMEM UUT (
@@ -16,10 +18,18 @@ module tb_DMEM ();
       .SIGN(SIGN),
       .ADDR(ADDR),
       .DATA_IN(DATA_IN),
-      .DATA_OUT()
+      .DATA_OUT(dout)
   );
 
-
+  task automatic print_dmem();
+    integer i;
+    $display("First 32 lines of DATA_MEMORY:");
+    for (i = 0; i < 32; i = i + 1) begin
+      $display("addr[%0d] = %08h", i, UUT.ram_64kb[i]);
+    end
+  endtask
+  /*
+* print out first 32 words anytime they change
   integer i;
   always @(posedge CLK) begin
     $write("Time %0t: RAM[0:31] = ", $time);
@@ -28,7 +38,7 @@ module tb_DMEM ();
     end
     $write("\n");
   end
-
+*/
 
   initial begin
     $dumpfile("tb_DMEM.vcd");
@@ -53,13 +63,13 @@ module tb_DMEM ();
     ADDR <= 14'b0;
     DATA_IN <= 32'hdeadbeef;
 
-    // WRITE
+    // WRITE WORD
     #10 RDEN <= 1'b0;
     WEN <= 1'b1;
     BYTE_SEL <= 2'b10;
     SIGN <= 1'b0;
     ADDR <= 14'b0;
-    DATA_IN <= 32'hdeadbeef;
+    DATA_IN <= 32'hdeadbee0;
 
     // READ
     #10 RDEN <= 1'b1;
@@ -67,7 +77,10 @@ module tb_DMEM ();
     BYTE_SEL <= 2'b10;
     SIGN <= 1'b0;
     ADDR <= 14'b0;
-    DATA_IN <= 32'h0000_0000;
+    #10 myvar <= dout;
+    #10 $display("ADDR = %08h", ADDR);
+    #10 $display("DATA_OUT = %08h", myvar);
+
 
     // WRITE
     #10 RDEN <= 1'b0;
@@ -75,7 +88,7 @@ module tb_DMEM ();
     BYTE_SEL <= 2'b00;
     SIGN <= 1'b0;
     ADDR <= 14'd4;
-    DATA_IN <= 32'hdeadbeef;
+    DATA_IN <= 32'hdeadbee4;
 
     // WRITE
     #10 RDEN <= 1'b0;
@@ -83,15 +96,15 @@ module tb_DMEM ();
     BYTE_SEL <= 2'b01;
     SIGN <= 1'b0;
     ADDR <= 14'd8;
-    DATA_IN <= 32'hdeadbeef;
+    DATA_IN <= 32'hdeadbee8;
 
     // WRITE
     #10 RDEN <= 1'b0;
     WEN <= 1'b1;
     BYTE_SEL <= 2'b00;
     SIGN <= 1'b1;
-    ADDR <= 14'd12;
-    DATA_IN <= 32'hdeadbeef;
+    ADDR <= 14'hc;
+    DATA_IN <= 32'hdeadbeec;
 
     // READ
     #10 RDEN <= 1'b1;
@@ -100,6 +113,9 @@ module tb_DMEM ();
     SIGN <= 1'b0;
     ADDR <= 14'd4;
     DATA_IN <= 32'h0000_0000;
+    #10 myvar <= dout;
+    #10 $display("ADDR = %08h", ADDR);
+    #10 $display("DATA_OUT = %08h", myvar);
 
     // READ
     #10 RDEN <= 1'b1;
@@ -108,6 +124,9 @@ module tb_DMEM ();
     SIGN <= 1'b0;
     ADDR <= 14'd8;
     DATA_IN <= 32'h0000_0000;
+    #10 myvar <= dout;
+    #10 $display("ADDR = %08h", ADDR);
+    #10 $display("DATA_OUT = %08h", myvar);
 
     // READ
     #10 RDEN <= 1'b1;
@@ -116,6 +135,9 @@ module tb_DMEM ();
     SIGN <= 1'b0;
     ADDR <= 14'd12;
     DATA_IN <= 32'h0000_0000;
+    #10 myvar <= dout;
+    #10 $display("ADDR = %08h", ADDR);
+    #10 $display("DATA_OUT = %08h", myvar);
 
     // READ
     #10 RDEN <= 1'b1;
@@ -124,6 +146,9 @@ module tb_DMEM ();
     SIGN <= 1'b1;
     ADDR <= 14'd8;
     DATA_IN <= 32'h0000_0000;
+    #10 myvar <= dout;
+    #10 $display("ADDR = %08h", ADDR);
+    #10 $display("DATA_OUT = %08h", myvar);
 
 
     // WRITE
@@ -141,8 +166,11 @@ module tb_DMEM ();
     SIGN <= 1'b0;
     ADDR <= 14'd1;
     DATA_IN <= 32'h0000_0000;
+    #10 myvar <= dout;
+    #10 $display("ADDR = %08h", ADDR);
+    #10 $display("DATA_OUT = %08h", myvar);
 
-
+    print_dmem();
 
     #20 $finish;
   end
